5fed751fa9072cf1c677037a71c9642f9dbb844a
shubham singh <43708880+shubh5900@users.noreply.github.com>
2023-11-19 21:42:51 +0530

    Delete BlackCoffer-main directory

--- BlackCoffer-main/DashBoard/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path("Login/",views.Login),
-    path("Home/",views.Home),
-    path('Apply/<int:id>/',views.Apply,name='Apply'),
-]
--- BlackCoffer-main/README.md
+++
@@ -1,8 +0,0 @@
-# BlackCoffer
-
-1. bootstrap
-2. Django
-3. python
-4. Html
-5. CSS
-6. chart.js
